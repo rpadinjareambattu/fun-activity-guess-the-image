ee6e57aa38219cff9ddb7cdce791e673ceeb911e
Sreedarsh <schandanampurath@opentrends.net>
2022-12-02 16:11:03 +0530

    card style and integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Button } from "react-bootstrap";
 import "./App.css";
-import Card from "./components/card";
+import ImageCard from "./components/card";
+import scratchImg from "./assets/images/scratch.jpeg";
+
+const data: any = [
+  {
+    qno: 1,
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e4/Alappuzha_Boat_Beauty_W.jpg",
+    answer: "img 1 ans",
+  },
+  {
+    qno: 2,
+    imgUrl:
+      "https://static.toiimg.com/thumb/msid-94248825,width-748,height-499,resizemode=4,imgsize-90508/.jpg",
+    answer: "img 2 ans",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <Card />
-      <p>smkfdkfsmfkdm</p>
-    </div>
+    <section>
+      <div className="container-fluid">
+        <div className="row">
+          {data.map((item: any, index: any) => {
+            console.log(item);
+            return (
+              <ImageCard
+                key={index}
+                qans={item.answer}
+                imgsrc={scratchImg}
+                itemno={item.qno}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,5 +1,15 @@
-const Card = () => {
-  return <h1>Card</h1>;
+import React from "react";
+import styles from "./index.module.scss";
+
+const ImageCard = (props: any) => {
+  return (
+    <div className="col-md-3">
+      <div className={styles.imagecard__img}>
+        <img className="img-fluid" src={props.imgsrc} />
+        <span>{props.itemno}</span>
+      </div>
+    </div>
+  );
 };
 
-export default Card;
+export default ImageCard;
