0930a161691c246a3dca515b733858dddc444201
sarathcr <sarathcr9177725@gmail.com>
2022-12-02 13:47:29 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import Card from "./components/card";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+      <p>smkfdkfsmfkdm</p>
+    </div>
+  );
+}
+
+export default App;
