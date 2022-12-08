8665059da42f3357396958b1ed9a6752f166ef20
scheerakkadan <scheerakkadan@opentrends.net>
2022-12-08 12:55:23 +0530

    deployment branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // root: path.resolve(__dirname, "src"),
+  // root: path.resolve(__dirname, "src"),
+
   plugins: [react()],
 });
