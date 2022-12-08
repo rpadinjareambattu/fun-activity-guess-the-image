f3fb88bd08792b2c398316447dacb695f7755683
scheerakkadan <scheerakkadan@opentrends.net>
2022-12-08 12:50:54 +0530

    updated assets folder in public folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // root: path.resolve(__dirname, "src"),
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        
-        assetFileNames: ({name}) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
-              return 'assets/images/[name][extname]';
-          }
-          
-          if (/\.css$/.test(name ?? '')) {
-              return 'assets/css/[name]-[hash][extname]';   
-          }
- 
-          // default value
-          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-          return 'assets/[name]-[hash][extname]';
-        },
-      },
-    }
-  },
 });
